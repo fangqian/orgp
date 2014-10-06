080bb8e4c6532e7f6191531ee1e21469a70ee85f
LeslieZhu <pythonisland@gmail.com>
2014-10-07 05:48:49 +0800

    will use cpp, not objc

---
+++ orgp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  orgp
+//
+//  Created by LeslieZhu on 14-10-7.
+//  Copyright (c) 2014年 LeslieZhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
